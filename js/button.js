fcc5f43c0612b9d763e840808799fcdba6e6c897
Hajime <42915482+Hajime4th@users.noreply.github.com>
2018-12-26 22:01:31 +0200

    Updated to V1.1.0! Alot of bugfixes.

--- js/button.js
+++ js/button.js
@@ -3,6 +3,10 @@
       const remote = require('electron').remote;
 
       function init() {
+        document.getElementById("min-btn").addEventListener("click", function (e) {
+          const window = remote.getCurrentWindow();
+          window.minimize();
+        });
 
         document.getElementById("close-btn").addEventListener("click", function (e) {
           const window = remote.getCurrentWindow();
